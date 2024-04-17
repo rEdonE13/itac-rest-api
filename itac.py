5bbb32250198afd5a2b13118bdca1872db2bdfd8
alfredol <alfredo.lugo@itacsoftware.com>
2024-04-17 10:41:46 +0200

    functions from tr module added.

--- itac.py
+++ itac.py
@@ -90,6 +90,82 @@ class ITAC:
         return res.json()["result"]["serialNumberResultValues"]
 
 
+    def trActivateWorkOrder(self,
+            station_number: str,
+            work_order: str,
+            serial_number: str,
+            serial_number_pos: str = "-1",
+            process_layer: int = 0,
+            flag: int = 1
+    ) -> list[str]:
+        """
+        This function activates or deactivates the passed work order on the station or line.
+        
+        Parameters
+            station_number (str): Station number of the work station in the iTAC system
+            work_order (str): Number of a production order
+            serial_number (str): Serial number of the panel
+            serial_number_pos (str): Position of the single panel in the unit-array
+            process_layer (int): Orientation of the PCB during the work step [0; 1; 2; 3]
+            flag (int): Mode "Work order activation" [1; 11; 21; 2; 12; 22; 3; 13; 23; 4; 14; 24]
+        """
+        url = self.api_url + "trActivateWorkOrder"
+        payload = {
+            "sessionContext":       self.session_context,
+            "stationNumber":        station_number,
+            "workOrderNumber":      work_order,
+            "serialNumber":         serial_number,
+            "serialNumberPos":      serial_number_pos,
+            "processLayer":         process_layer,
+            "flag":                 flag
+        }
+
+        res = requests.post(url=url, data=json.dumps(payload), headers=HEADERS, timeout=5)
+        res.raise_for_status()
+        return res.json()["result"]
+
+
+    def trUploadState(self,
+        station_number: str,
+        process_layer: int,
+        serial_number: str,
+        serial_number_pos: str,
+        serial_number_state: int,
+        duplicate_serial_number: int,
+        result_keys: list[str]
+    ) -> list[str]:
+        """
+        This function books, in a work-step-conformant manner, the process state of one or more
+        individual products (single panel) or of a complete multiple panel. In doing so, a time 
+        stamp is set for the component lot capture. This function is the universal function for 
+        the booking of a product.
+
+        Parameters
+            station_number (str): Station number of the work station in the iTAC system
+            process_layer (int): Orientation of the PCB during the work step [0; 1; 2]
+
+        """
+        url = self.api_url + "trUploadState"
+        payload = {
+            "sessionContext":           self.session_context,
+            "stationNumber":            station_number,
+            "processLayer":             process_layer,
+            "serialNumberRef":          serial_number,
+            "serialNumberRefPos":       serial_number_pos,
+            "serialNumberState":        serial_number_state,
+            "duplicateSerialNumber":    duplicate_serial_number,
+            "bookDate":                 "",
+            "cycleTime":                "",
+            "serialNumberUploadKeys":   "",
+            "serialNumberUploadValues": "",
+            "serialNumberResultValues": ""
+        }
+
+        res = requests.post(url=url, data=json.dumps(payload), headers=HEADERS, timeout=5)
+        res.raise_for_status()
+        return res.json()["result"]
+
+
     def getAttributesFromSerialNumber(self,
             serial_number: str,
             attributes) -> list[str] :
@@ -111,13 +187,75 @@ class ITAC:
         return res.json()["result"]["attributeResultValues"]
 
 
-    def trGetStationSetting(self, station_setting_result_keys: list[str]) -> list[str]:
+    def trGetNextSerialNumber(self,
+            station_number: str,
+            work_order: str,
+            part_number: str,
+            number_of_records: int) -> list[str] :
+        """
+        This function returns the serial numbers provided by the iTAC system for a part 
+        that is to be produced.
+
+        Parameters
+            station_number (str): Station number of the work station in the iTAC system
+            work_order (str): Number of a production order; if necessary, with the ID alternative that is to be used for the ID generator
+            part_number (str): Part number in the iTAC system
+            number_of_records (int): Default value for the number of entries in the array
+        """
+        url = self.api_url + "trGetNextSerialNumber"
+        payload = {
+            "sessionContext":       self.session_context,
+            "stationNumber":        station_number,
+            "workOrderNumber":      work_order,
+            "partNumber":           part_number,
+            "numberOfRecords":      number_of_records
+        }
+
+        res = requests.post(url=url, data=json.dumps(payload), headers=HEADERS, timeout=5)
+        res.raise_for_status()
+        return res.json()["result"]["serialNumberArray"]
+
+
+    def trAssignSerialNumberForProductOrWorkOrder(self,
+            station_number: str,
+            work_order: str,
+            part_number: str,
+            bom_version: str,
+            serial_number_ref: str,
+            serial_number_ref_pos: str,
+            process_layer: int,
+            serial_numbers_data: list,
+            activate_work_order: int
+        ) -> list[str] :
+        """Get next serial number from ID Generator."""
+        url = self.api_url + "trAssignSerialNumberForProductOrWorkOrder"
+        payload = {
+            "sessionContext":       self.session_context,
+            "stationNumber":        station_number,
+            "workOrderNumber":      work_order,
+            "partNumber":           part_number,
+            "bomVersion":           bom_version,
+            "serialNumberRef":      serial_number_ref,
+            "serialNumberRefPos":   serial_number_ref_pos,
+            "processLayer":         process_layer,
+            "serialNumberArray":     serial_numbers_data,
+            "activateWorkOrder":    activate_work_order
+        }
+
+        res = requests.post(url=url, data=json.dumps(payload), headers=HEADERS, timeout=5)
+        res.raise_for_status()
+        return res.json()["result"]
+
+
+    def trGetStationSetting(self, 
+            station_number: str,
+            station_setting_result_keys: list[str]) -> list[str]:
         """Queries the actual product version and the corresponding
         work order at a station."""
         url = self.api_url + "trGetStationSetting"
         payload = {
             "sessionContext":           self.session_context,
-            "stationNumber":            station["id"],
+            "stationNumber":            station_number,
             "stationSettingResultKeys": station_setting_result_keys
         }
 
